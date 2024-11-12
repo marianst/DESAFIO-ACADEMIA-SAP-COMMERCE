140b35e539da6aff9ef3d7cfcf14b213a0e0920f
marianst <mariana_anst@hotmail.com>
2024-11-11 22:28:59 -0300

    Add files via upload

---
+++ escola-online/src/main/java/com/escola/escola_online/EscolaOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.escola.escola_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolaOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolaOnlineApplication.class, args);
+	}
+
+}
